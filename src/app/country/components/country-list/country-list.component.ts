a297022eeab9f44f865e8c62d4b7a18533f5826e
PonssDev <alanponstrabajo@gmail.com>
2025-05-14 12:28:09 +0200

    feat: show countries on table

---
+++ src/app/country/components/country-list/country-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+import { RESTCountry } from '../../interfaces/rest-countries.interface';
+
+@Component({
+  selector: 'country-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './country-list.component.html',
+})
+export class CountryListComponent {
+  countries = input.required<RESTCountry[]>()
+}
--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { SearchInputComponent } from "../../components/search-input/search-input.component";
+import { CountryListComponent } from "../../components/country-list/country-list.component";
+import { CountryService } from '../../services/country.service';
+import { RESTCountry } from '../../interfaces/rest-countries.interface';
 
 @Component({
   selector: 'app-by-capital-page',
   standalone: true,
-  imports: [],
+  imports: [SearchInputComponent, CountryListComponent],
   templateUrl: './by-capital-page.component.html',
 })
-export class ByCapitalPageComponent { }
+export class ByCapitalPageComponent {
+  public readonly countries = signal<RESTCountry[]>([])
+
+  private readonly isLoading = signal(false)
+
+  private readonly isError = signal<string | null>(null)
+
+  private readonly countryService = inject(CountryService)
+
+  onSearch(query: string): void {
+
+    if (this.isLoading()) return
+
+    this.isLoading.set(true)
+    this.isError.set(null)
+
+    this.countryService.searchByCapital(query)
+      .subscribe((countries: RESTCountry[]) => {
+        this.isLoading.set(false)
+        this.countries.set(countries)
+
+        console.log(countries)
+      })
+  }
+}
