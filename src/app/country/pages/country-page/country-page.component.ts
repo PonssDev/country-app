4a534e23dd12870c4d01460706114d1b3f6103dc
PonssDev <alanponstrabajo@gmail.com>
2025-05-26 10:48:48 +0200

    feat: implementar carga de país con manejo de errores y estado de carga

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,17 +1,50 @@
-import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-country-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './country-page.component.html'
 })
 export class CountryPageComponent {
-  public countryCode = inject(ActivatedRoute).snapshot.params['code']
+  public countryCode = inject(ActivatedRoute).snapshot.params['code'];
+  private readonly countryService = inject(CountryService);
 
-  private readonly countryService = inject(CountryService)
+  // Signals para manejar el estado
+  public country = signal<Country | null>(null);
+  public isLoading = signal(true);
+  public hasError = signal(false);
 
+  constructor() {
+    this.loadCountry();
+  }
 
+  loadCountry() {
+    if (!this.countryCode) {
+      this.hasError.set(true);
+      this.isLoading.set(false);
+      return;
+    }
+
+    this.countryService.searchCountryByAlphaCode(this.countryCode)
+      .subscribe({
+        next: (country) => {
+          if (country) {
+            this.country.set(country);
+          } else {
+            this.hasError.set(true);
+          }
+          this.isLoading.set(false);
+        },
+        error: () => {
+          this.hasError.set(true);
+          this.isLoading.set(false);
+        }
+      });
+  }
 }
+
